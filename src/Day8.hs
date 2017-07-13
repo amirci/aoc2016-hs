2d30ff3ae9d47472b24f8343eaa378f8980bfdd9
Amir Barylko <amir@barylko.com>
2017-07-13 08:29:09 -0700

    Added rotation

--- src/Day8.hs
+++ src/Day8.hs
@@ -5,14 +5,17 @@ import Debug.Trace
 import Text.Parsec 
 import Data.Bifunctor
 import Data.Matrix
+import qualified Data.Vector as Vec
 
 bOff = '.'
 bOn  = '#'
+totalRows = 5
+totalCols = 60
 
 type Screen = Matrix Char
 type ScreenFn = Screen -> Screen
 
-empty = matrix 5 60 (const bOff)
+empty = matrix totalRows totalCols (const bOff)
 
 mkScreen :: [String] -> Screen
 mkScreen = foldl (flip apply) empty
@@ -20,12 +23,17 @@ mkScreen = foldl (flip apply) empty
 countPixels :: Screen -> Int
 countPixels = length . filter (== bOn) . toList
 
-apply :: String -> Screen -> Screen
+apply :: String -> ScreenFn
 apply instr = unwrap $ parse rectParser "(unknown)" instr
   where
     unwrap = either (const id) id
 
-turnOnRect :: Int -> Int -> Screen -> Screen
+shift :: Int -> [a] -> [a]
+shift n = reverse . shift' . reverse
+  where shift' l = drop n l ++ take n l
+
+-- rect nxm
+turnOnRect :: Int -> Int -> ScreenFn
 turnOnRect w h s = foldl setM s pts
   where 
     setM = flip $ setElem bOn
@@ -39,4 +47,27 @@ rectParser = do
   char 'x'
   height <- read <$> many1 digit
   return $ turnOnRect width height
+
+-- rotate column x=1 by 1
+rotateCol :: Int -> Int -> ScreenFn
+rotateCol col n s = replace shifted
+  where
+    shifted = shift n $ Vec.toList $ getCol (col + 1) s
+    replace col = foldl setM s $ zip col [1..totalRows]
+    setM s (e, row) = setElem e (row, col + 1) s
+
+rotateColParser :: Parsec String st ScreenFn
+rotateColParser = do
+  string "rotate"
+  spaces
+  string "column"
+  spaces
+  char 'x'
+  char '='
+  col <- read <$> many1 digit
+  spaces
+  string "by"
+  spaces
+  n <- read <$> many1 digit
+  return $ rotateCol col n
 
